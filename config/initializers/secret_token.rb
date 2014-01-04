d265d88556778cfbc14722d4446a028bd8b414d2
John <jsrratt@aol.com>
2014-01-04 16:47:40 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobseekers::Application.config.secret_token = '053e6f83618c258c244905fc991a25b08fc7a22985464507ee7281de0df0a55da104172b6d96ca8066be40634bb115c7e8cd035c7ec593b1ce827f2fa6d4b92f'
