fabc0695957d4e46b6e65907ad1720a705860c83
John <jsrratt@aol.com>
2014-01-05 18:46:00 -0500

    Added create and update CRUD actions

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -8,8 +8,33 @@ class AnnouncementsController < ApplicationController
   end
 
   def new
+    @announcement = Announcement.new
   end
 
-  def edit
+def create
+  @announcement = Announcement.new(params[:announcement])
+  if @announcement.save
+    flash[:notice] = "Job Announcement was saved."
+    redirect_to @announcement
+  else
+    flash[:error] = "There was an error saving the job announcement. Please try again."
+    render :new
   end
+end
+
+def edit
+@announcement = Announcement.find(params[:id])
+end
+
+  def update
+    @announcement = announcement.find(params[:id])
+    if @announcement.update_attributes(params[:announcement])
+      flash[:notice] = "Job Announcement was updated."
+      redirect_to @announcement
+    else
+      flash[:error] = "There was an error saving the Job Announcement. Please try again."
+      render :edit
+    end
+
+end
 end
