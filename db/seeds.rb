e93b97f8e660c73fecdd96e43b14523bb7588076
John <jsrratt@aol.com>
2014-01-12 20:13:43 -0500

    Jobseekers updated 1.12.13

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 u = User.first
 u.skip_reconfirmation!
 u.update_attributes(email: 'jsrratt@aol.com', password: 'helloworld', password_confirmation: 'helloworld')
-
+u.save
 puts "Seed finished"
 puts "#{User.count} users created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Announcement.count} announcments created"
+puts "#{Note.count} notes created"
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
